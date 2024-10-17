6ec9779737abe70eaf5b7a195abc1870599ef364
ramazonLider <ustau2453@gmail.com>
2024-10-17 17:36:07 +0500

    fwe
    few

--- core/settings.py
+++ core/settings.py
@@ -122,17 +122,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_URL = '/static/'
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Collect static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Your static files location
-]
-
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
